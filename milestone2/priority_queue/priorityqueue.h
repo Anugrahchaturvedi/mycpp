1b0c931bf000fe59cbf06ef701e1df76e4ee04f7
Anugrahchaturvedi <kushal.kush867@gmail.com>
2023-01-19 22:05:50 +0530

    19/1/23

---
+++ milestone2/priority_queue/priorityqueue.h
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+
+class priorityQueue{
+vector<int> pq;
+
+public:
+priorityQueue(){}
+bool isempty(){
+    return pq.size()==0;
+}
+//return cuurent size of priority queue
+int getSize(){
+return pq.size();
+}
+
+int getMin(){
+    if(isempty()){
+        return 0;
+    }
+    return pq[0];
+}
+void insert(int element){
+    pq.push_back(element);
+
+    int childIndex=pq.size()-1;
+    while(childIndex>0){
+    int parentIndex= (childIndex-1)/2;
+
+
+    if(pq[childIndex] < pq[parentIndex]){
+        int temp=pq[childIndex];
+        pq[childIndex]=pq[parentIndex];
+        pq[parentIndex]=temp;
+    }
+    else{
+        break;
+    }
+
+    childIndex=parentIndex;
+    }
+}
+
+};
